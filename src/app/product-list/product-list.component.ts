bc49c6d93f84692976f9052a35871f221004e024
upup297 <770025090@qq.com>
2019-10-09 18:26:56 +0800

    3 files modified, 14 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -3,15 +3,33 @@ import { Component } from '@angular/core';
 import { products } from '../products';
 
 @Component({
+  template:`
+    <input #newHero
+      (keyup.enter="addHero(newHero.value)"
+
+      (blur)="addHero(newHero.value); newHero.value=''">
+    <button (click)="addHero(newHero.value)">Add</button>
+    <ul>
+      <li *ngFor="let hero of heroes">{{hero}}</li>
+    </ul>
+  `,
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
   products = products;
-
+  heroes = ['Windstorm','Bombasto','magneta','Tornado'];
+  addHero(newHero:string){
+    if(newHero){
+      this.heroes.push(newHero)
+    }
+  }
   share() {
     window.alert('The product has been shared!');
+  }
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
